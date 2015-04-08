4a232d8733da643b229175fc5961de909916a4a0
Laszlo <tihanyil@tihanyilaszlo.com>
2015-04-08 11:44:19 +0200

    add new files

---
+++ Scraper.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Yii2 scraper wrapper
+ * With RESTful support.
+ *
+ * @category  Web-yii2
+ * @package   yii2-scraper
+ * @author    Laszlo Tihanyi <tihanyil73@gmail.com>
+ * @copyright 2015 Laszlo Tihanyi <tihanyil73@gmail.com>
+ * @license   http://opensource.org/licenses/MIT MIT Public
+ * @version   1.0.0
+ * @link      http://www.tihanyilaszlo.com
+ *
+ */
+
+namespace tihanyil\yii2\scraper;
+
+use Yii;
+use yii\base\Exception;
+use yii\helpers\Json;
+use yii\web\HttpException;
+
+/**
+ * scraper class
+ */
+class Scraper
+{
+}
